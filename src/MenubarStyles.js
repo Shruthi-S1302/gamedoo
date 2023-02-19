107bdfc82db8813a0a5238b005c67f6e41e5ca24
Shruthi-S1302 <89682079+Shruthi-S1302@users.noreply.github.com>
2023-02-19 18:55:07 +0530

    logout_button

--- src/MenubarStyles.js
+++ src/MenubarStyles.js
@@ -66,6 +66,7 @@ export const MenubarStyle = styled.div`
     position: fixed;
     bottom: 2%;
     left: 8%;
+    transform: translateX(-50%);
     display: flex;
     flex-direction: column;
     align-items: center;
